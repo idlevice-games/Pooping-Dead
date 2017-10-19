aa7c4653b539142fce063577766a9d2aa633f89e
Andrew Harris <idlevicegames@gmail.com>
2017-10-19 15:04:06 -0400

    Rewrite for connections
    Can now connect to left or right, but only one connection per side per component.

--- Assets/Scripts/Clickspawn.cs
+++ Assets/Scripts/Clickspawn.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Clickspawn : MonoBehaviour {
 
-    public GameObject Brick;
+    public GameObject Brick;        
     public GameObject BoatManager;
 
     private int childCount;
@@ -12,42 +12,76 @@ public class Clickspawn : MonoBehaviour {
 
     void Update()
     {
+
+        
         if (Input.GetMouseButtonDown(0))
         {
             childCount = BoatManager.transform.childCount;
 
             if (childCount == 0)
             {
-                GameObject brick = Instantiate(Brick, Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10)), Brick.transform.rotation) as GameObject;
-
-                //brick.transform.Rotate(0, 0, UnityEngine.Random.Range(0, 0));
-                //brick.transform.localScale = new Vector3(UnityEngine.Random.Range(1f, 2f),0.6f,1)*0.4f;
-                brick.GetComponent<Rigidbody2D>().mass = brick.transform.localScale.x * brick.transform.localScale.y * 5f;
-                //brick.AddComponent<FixedJoint2D>();
+                GameObject brick = Instantiate(Brick, Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10)), Brick.transform.rotation) as GameObject;        
+                //brick.GetComponent<Rigidbody2D>().mass = brick.transform.localScale.x * brick.transform.localScale.y * 5f;
+                brick.GetComponent<Rigidbody2D>().mass = 0.2f;
                 brick.transform.parent = BoatManager.transform;
-
-                //BoatManager.AddComponent<Rigidbody2D>();
-                //BoatManager.GetComponent<Rigidbody2D>().useAutoMass = true;
-                //brick.AddComponent<FixedJoint2D>();
-                //brick.GetComponent<FixedJoint2D>().connectedBody = BoatManager.GetComponent<Rigidbody2D>();
-
-                //BoatManager.transform.SetPositionAndRotation(brick.transform.position, Brick.transform.rotation);
-
             }
             else
             {
-                Debug.Log("Child count: " + childCount);
-                lastChild = BoatManager.transform.GetChild(childCount - 1).gameObject;
+                Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+                RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
 
+                if (hit)
+                {
+                    //Debug.Log("Object type clicked on: " + hit.transform.gameObject.name);
+                    if (hit.transform.gameObject.name.Substring(0, 13).Equals("HullComponent"))
+                    {
+                        //Debug.Log("Object..: " + hit.transform.gameObject);
+                        lastChild = hit.transform.gameObject;
 
-                GameObject brick = Instantiate(Brick, lastChild.transform.localPosition + new Vector3(1, 0, 0), Brick.transform.rotation) as GameObject;
-                brick.AddComponent<FixedJoint2D>();
-                brick.GetComponent<FixedJoint2D>().connectedBody = lastChild.GetComponent<Rigidbody2D>();
-                brick.GetComponent<FixedJoint2D>().connectedAnchor = new Vector2(0.0f, -0.0f);
-                brick.GetComponent<FixedJoint2D>().breakForce = 128.0f;                
-                brick.transform.parent = BoatManager.transform;
+                        // Determine what side of the hull component is hit in order to determine where to attach the next component
+                        if (hit.collider.offset.x >= 0.0f)
+                        {
+                            if (lastChild.GetComponent<HullComponent>().rightSide == null)
+                            {
+                                GameObject componentObject = new GameObject();
+                                componentObject = Instantiate(Brick, lastChild.transform.localPosition + new Vector3(0.65f, 0, 0), Brick.transform.rotation) as GameObject;
+                                lastChild.GetComponent<HullComponent>().rightSide = componentObject;
+                                componentObject.GetComponent<HullComponent>().leftSide = lastChild;
+                                componentObject.AddComponent<FixedJoint2D>();
+                                componentObject.GetComponent<FixedJoint2D>().connectedBody = lastChild.GetComponent<Rigidbody2D>();
+                                componentObject.GetComponent<FixedJoint2D>().connectedAnchor = new Vector2(0.0f, -0.0f);
+                                componentObject.GetComponent<FixedJoint2D>().breakForce = 128.0f;
+                                componentObject.transform.parent = BoatManager.transform;
+                            }
+                            else
+                            {
+                                Debug.Log("Cannot connect to the right side, already have a joint to another component..");
+                            }
+                        }
+                        else
+                        {
+                            if (lastChild.GetComponent<HullComponent>().leftSide == null)
+                            {
+                                GameObject componentObject = new GameObject();
+                                componentObject = Instantiate(Brick, lastChild.transform.localPosition + new Vector3(-0.65f, 0, 0), Brick.transform.rotation) as GameObject;
+                                lastChild.GetComponent<HullComponent>().leftSide = componentObject;
+                                componentObject.GetComponent<HullComponent>().rightSide = lastChild;
+                                componentObject.AddComponent<FixedJoint2D>();
+                                componentObject.GetComponent<FixedJoint2D>().connectedBody = lastChild.GetComponent<Rigidbody2D>();
+                                componentObject.GetComponent<FixedJoint2D>().connectedAnchor = new Vector2(0.0f, -0.0f);
+                                componentObject.GetComponent<FixedJoint2D>().breakForce = 128.0f;
+                                componentObject.transform.parent = BoatManager.transform;
 
-            }
+                            }
+                            else
+                            {
+                                Debug.Log("Cannot connect to the left side, already have a joint to another component..");
+                            }
+                        }
+                    }                   
+                }
+                
+            }            
         }
         if (Input.GetMouseButtonDown(1))
         {
@@ -62,3 +96,4 @@ public class Clickspawn : MonoBehaviour {
         
     }
 }
+    
---
+++ Assets/Scripts/HullComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HullComponent : MonoBehaviour {
+
+    public GameObject leftSide;
+    public GameObject rightSide;
+    public GameObject topSide;
+    public GameObject bottomSide;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
